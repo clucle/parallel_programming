7712b5e067f5088102448c67d6271b40aa4937a8
clucle <wjdenwls123@gmail.com>
2019-09-21 02:36:15 +0900

    feat: external merge sort
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- project1/main.cpp
+++ project1/main.cpp
@@ -9,6 +9,7 @@
 #include <cstdio>
 #include <chrono>
 #include <semaphore.h>
+#include <queue>
 
 #define MAX_NUM_THREADS 40
 #define SIZE_KEY 10
@@ -21,11 +22,24 @@ struct KeyValue {
 };
 
 
-int cmp(const KeyValue& me, const KeyValue &other) {
+bool cmp(const KeyValue& me, const KeyValue &other) {
 	return memcmp(me.key, other.key, SIZE_KEY) <= 0;
 }
 
-const int LIMIT_BY_RAM_BLOCK_SIZE = 1900000;
+struct KeyValueNode {
+	char key[10];
+	char payload[90];
+	int file_idx = 0;
+	int cur_g = 0;
+	int end_g = 0;
+	ifstream* ifs;
+};
+
+bool operator<(const KeyValueNode &me, const KeyValueNode &other) {
+	return memcmp(me.key, other.key, SIZE_KEY) > 0;
+}
+
+const int LIMIT_BY_RAM_BLOCK_SIZE = 19000000;
 
 mutex g_key_mutex;
 sem_t sem;
@@ -39,11 +53,10 @@ void gen_divided_sort_file(string file_read_name, int idx, int start, int end) {
 	sem_wait(&sem);
 	struct KeyValue kv;
 	vector<KeyValue> v;
-		
 	ifstream ifs(file_read_name, ios::binary | ios::in);
 	if (ifs.is_open()) {
-		ifs.seekg(start);
-		for (int i = start; i < end; i+=sizeof(struct KeyValue)) {
+		for (int i = start; i < end; i++) {
+			ifs.seekg(i * 100);
 			ifs.read(&kv.key[0], sizeof(struct KeyValue));
 			v.push_back(kv);
 		}
@@ -51,15 +64,16 @@ void gen_divided_sort_file(string file_read_name, int idx, int start, int end) {
 	ifs.close();
 
 	sort(v.begin(), v.end(), cmp);
-	
+
+	remove(gen_tmp_name(idx).c_str());
 	ofstream ofs(gen_tmp_name(idx), ios::binary | ios::out);
 	for (auto e: v) {
-		ofs.write((char*)&kv, sizeof(struct KeyValue));
+		ofs.write((char*)&e, sizeof(struct KeyValue));
 	}
 	ofs.close();
-	
+
 	v.clear();
-	
+
 	sem_post(&sem);
 }
 
@@ -80,15 +94,12 @@ int main(int argc, char* argv[]) {
 	int cnt_keyvalue = (long long)ifs.tellg() / (long long)sizeof(struct KeyValue);
 	ifs.close();
 	int block_size = cnt_keyvalue / MAX_NUM_THREADS;
-	
-		cout << block_size << " bszie " << '\n';
-	while (block_size * MAX_NUM_THREADS >= LIMIT_BY_RAM_BLOCK_SIZE) {
 
-		cout << block_size << " bszie " << '\n';
+	while (block_size * MAX_NUM_THREADS >= LIMIT_BY_RAM_BLOCK_SIZE) {
 		block_size /= 2;
 		block_size -= block_size % 100;
 	}
-	cout << block_size << " bszie " << '\n';
+
 	// generate divdided file
 	sem_init(&sem, 0, MAX_NUM_THREADS);
 
@@ -97,9 +108,9 @@ int main(int argc, char* argv[]) {
 		int end = min((i + 1) * block_size, cnt_keyvalue);
 		thread t = thread(gen_divided_sort_file, file_read_name, i, i * block_size, end);
 		vt.push_back(std::move(t));
-		cout << i * block_size << ' ' << end << '\n';
 		if (end == cnt_keyvalue) break;
 	}
+
 
 	int cnt_file = vt.size();
 
@@ -107,10 +118,40 @@ int main(int argc, char* argv[]) {
 		vt.at(i).join();
 	}
 
-
 	sem_destroy(&sem);
 
 	// merge sort by divided file
+	priority_queue<KeyValueNode> pq;
 
+	ofstream ofs(file_write_name, ios::binary | ios::out);
+	for (int i = 0; i < cnt_file; i++) {
+		KeyValueNode kvn;
+		kvn.file_idx = i;
+		kvn.ifs = new ifstream(gen_tmp_name(i), ios::binary | ios::in);
+		kvn.ifs->seekg(0, ios::end);
+		kvn.cur_g = 0;
+		kvn.end_g = (long long)kvn.ifs->tellg() / (long long)sizeof(struct KeyValue);
+		kvn.ifs->seekg(0);
+		kvn.ifs->read(&kvn.key[0], sizeof(struct KeyValue));
+		pq.push(std::move(kvn));
+	}
+
+	while (!pq.empty()) {
+		auto kvn = pq.top();
+		pq.pop();
+		ofs.write((char*)&kvn, sizeof(struct KeyValue));
+		kvn.cur_g++;
+		if (kvn.cur_g == kvn.end_g) {
+			delete kvn.ifs;
+			remove(gen_tmp_name(kvn.file_idx).c_str());
+			continue;
+		}
+		kvn.ifs->seekg(kvn.cur_g * 100);
+		kvn.ifs->read(&kvn.key[0], sizeof(struct KeyValue));
+		pq.push(std::move(kvn));
+	}
+
+	ofs.close();
+	return 0;
 }
 
