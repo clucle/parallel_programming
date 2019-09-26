77e74b3a5f97e1fd5c1ac976ecd5c24a66330d44
clucle <wjdenwls123@gmail.com>
2019-09-27 01:38:14 +0900

    feat: add condition variable at global load thread
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- project1/main.cpp
+++ project1/main.cpp
@@ -8,8 +8,8 @@
 #include <mutex>
 #include <cstdio>
 #include <chrono>
-#include <semaphore.h>
 #include <queue>
+#include <condition_variable>
 
 #define MAX_NUM_THREADS 40
 #define SIZE_KEY 10
@@ -40,8 +40,17 @@ bool operator<(const KeyValueNode &me, const KeyValueNode &other) {
 
 const int LIMIT_BY_RAM_BLOCK_SIZE = 19000000;
 
-mutex g_key_mutex;
-sem_t sem;
+
+mutex g_main_mutex;
+condition_variable g_main_cv;
+thread g_load_thread[MAX_NUM_THREADS];
+mutex g_load_mutex;
+condition_variable g_load_cv;
+string g_file_read_name;
+unsigned int g_kv_idx[MAX_NUM_THREADS];
+unsigned int g_kv_idx_file[MAX_NUM_THREADS];
+unsigned int g_kv_cnt[MAX_NUM_THREADS];
+int g_kv_worked;
 
 string gen_tmp_name(int idx) {
 	return "tmp" + to_string(idx) + ".data";
@@ -50,28 +59,55 @@ string gen_tmp_name(int idx) {
 struct KeyValue kv[19000001];
 priority_queue<KeyValueNode> pq;
 
-void load_file(string file_read_name, unsigned int idx, unsigned int idx_file, unsigned int cnt) {
-	ifstream ifs(file_read_name, ios::binary | ios::in);
-	unsigned int offset = idx_file * sizeof(KeyValue);
-	ifs.seekg(offset);
-	ifs.read(&kv[idx].key[0], sizeof(struct KeyValue) * cnt);
-	ifs.close();
+void load_file(int tid) {
+	while (1) {
+		{	
+			unique_lock<mutex> lk(g_load_mutex);
+			g_load_cv.wait(lk);
+			lk.unlock();
+			if (g_kv_idx[0] == -1) return ;
+		}
+		// init
+		string file_read_name = g_file_read_name;
+		unsigned int idx = g_kv_idx[tid];
+		unsigned int idx_file = g_kv_idx_file[tid];
+		unsigned int cnt = g_kv_cnt[tid];
+
+		// load file
+		ifstream ifs(file_read_name, ios::binary | ios::in);
+		unsigned int offset = idx_file * sizeof(KeyValue);
+		ifs.seekg(offset);
+		ifs.read(&kv[idx].key[0], sizeof(struct KeyValue) * cnt);
+		ifs.close();
+		__atomic_fetch_add(&g_kv_worked, 1, __ATOMIC_SEQ_CST);
+		if (g_kv_worked == MAX_NUM_THREADS) {
+			unique_lock<mutex> lk(g_main_mutex);
+			g_main_cv.notify_one();
+			lk.unlock();
+		}
+	}
 }
 
-void load_file_multi_thread(string file_read_name, unsigned int idx, unsigned int idx_file, unsigned int cnt) {
-	thread t[MAX_NUM_THREADS];
+void load_file_multi_thread(string file_read_name, unsigned int idx, unsigned int idx_file, unsigned int cnt) {	
+	unique_lock<mutex> lk(g_main_mutex);
 	int cnt_per_thread = cnt / MAX_NUM_THREADS;
+	g_file_read_name = file_read_name;
 	for (int i = 0; i < MAX_NUM_THREADS - 1; i++) {
 		int pos = i * cnt_per_thread;
-		t[i] = thread(load_file, file_read_name, idx + pos, idx_file + pos, cnt_per_thread);
+		g_kv_idx[i] = idx + pos;
+		g_kv_idx_file[i] = idx_file + pos;
+		g_kv_cnt[i] = cnt_per_thread;
 	}
 	int pos = (MAX_NUM_THREADS - 1) * cnt_per_thread;
 	int last_cnt = cnt - cnt_per_thread * (MAX_NUM_THREADS - 1);
-	t[MAX_NUM_THREADS - 1] = thread(load_file, file_read_name, idx + pos, idx_file + pos, last_cnt);
+	g_kv_idx[MAX_NUM_THREADS - 1] = idx + pos;
+	g_kv_idx_file[MAX_NUM_THREADS - 1] = idx_file + pos;
+	g_kv_cnt[MAX_NUM_THREADS - 1] = last_cnt;
 
-	for (int i = 0; i < MAX_NUM_THREADS; i++) {
-		t[i].join();
-	}
+	g_kv_worked = 0;
+	g_load_cv.notify_all();
+	g_main_cv.wait(lk);
+	lk.unlock();
 }
 
 void gen_divided_sort_file(string file_read_name, string file_write_name, unsigned int start, unsigned int end) {
@@ -98,11 +134,23 @@ void load_data_at_and_push_pq(int file_idx, int each_space) {
 	pq.push(std::move(kvn));
 }
 
+void join_global_load_thread() {
+	g_kv_idx[0] = -1;
+	g_load_cv.notify_all();
+	for (int i = 0; i < MAX_NUM_THREADS; i++) {
+		g_load_thread[i].join();
+	}
+}
+
 int main(int argc, char* argv[]) {
 
 	if (argc < 3) {
 		cout << "usage: ./run InputFile OutputFile\n";
 		return 0;
+	}
+
+	for (int i = 0; i < MAX_NUM_THREADS; i++) {
+		g_load_thread[i] = thread(load_file, i);
 	}
 	string file_read_name = argv[1];
 	string file_write_name = argv[2];
@@ -115,6 +163,7 @@ int main(int argc, char* argv[]) {
 
 	if (cnt_keyvalue <= 15000000) {
 		gen_divided_sort_file(file_read_name, file_write_name, 0, cnt_keyvalue);
+		join_global_load_thread();
 		return 0;
 	}
 
@@ -176,6 +225,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	ofs.close();
+	join_global_load_thread();
 	return 0;
 }
 
