a5c95d8335a4a776bb7fff2b319ae61c1a9ea2f8
clucle <wjdenwls123@gmail.com>
2019-10-20 23:32:44 +0900

    feat: add edges for check deadlock
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- project2/database.cpp
+++ project2/database.cpp
@@ -2,6 +2,14 @@
 
 Database::Database(int N, int R, int E) : sz(R)
 {
+    edges_out = new std::set<int>[N + 1];
+    edges_in = new std::set<int>[N + 1];
+}
+
+Database::~Database()
+{
+    delete[] edges_out;
+    delete[] edges_in;
 }
 
 std::mutex &Database::get_lock()
--- project2/database.hpp
+++ project2/database.hpp
@@ -2,17 +2,21 @@
 #define C_DATABASE
 
 #include <mutex>
+#include <set>
 
 class Database
 {
 public:
     Database(int N, int R, int E);
+    ~Database();
     size_t size();
     std::mutex &get_lock();
 
 private:
     std::mutex m;
     size_t sz;
+    std::set<int> *edges_out;
+    std::set<int> *edges_in;
 };
 
 #endif
