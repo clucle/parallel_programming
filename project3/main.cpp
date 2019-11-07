f286f7252a9342cd2ba1205cef385040e8539959
clucle <wjdenwls123@gmail.com>
2019-11-07 16:25:37 +0900

    feat: add main
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- project3/main.cpp
+++ project3/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <cstdlib>
+#include <thread>
+#include <chrono>
+
+#include "worker.hpp"
+#include "snapshot.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +16,27 @@ int main(int argc, char *argv[])
 
     int N = atoi(argv[1]);
 
-    // TODO:
-    // Make worker thread and update snapshot
+    std::unique_ptr<WaitFreeSnapshot> snapshot(new WaitFreeSnapshot(N));
+    std::vector<Worker> v_worker;
+
+    for (int tid = 0; tid < N; tid++)
+    {
+        v_worker.emplace_back(tid, snapshot);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        v_worker[i].run_thread();
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(60));
+
+    for (int i = 0; i < N; i++)
+    {
+        v_worker[i].join_thread();
+    }
+
+    std::cout << "Update Cnt : "<< snapshot->get_cnt() << '\n';
+
+    return 0;
 }
